792212f97b114e9f954fa3a0fe57811309641545
aubincspitzer <59554139+aubincspitzer@users.noreply.github.com>
2022-01-05 12:06:11 -0800

    test

---
+++ stories/Title/Title.stories.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import { Title } from "./index";
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: "WDC/Title",
+  component: Title
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+};
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template = (args) => <Title {...args} />;
+
+export const Primary = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Primary.args = {
+  content: "Example Title"
+};
