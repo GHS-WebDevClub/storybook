4594d8fc582eb5193f294ed9ffe43c34d03c8414
aubincspitzer <59554139+aubincspitzer@users.noreply.github.com>
2022-03-22 23:25:26 -0700

    Added IconButton component

---
+++ ui/01_atoms/icon_button/IconButton.stories.tsx
@@ -0,0 +1,32 @@
+import { IconButton, IconButtonProps } from "./IconButton";
+
+export default {
+    title: "Atoms/IconButton",
+    component: IconButton,
+};
+
+const Template = (args: IconButtonProps) => <IconButton {...args} />;
+
+export const Primary = Template.bind({  })
+Primary.args = {
+    primary: true
+}
+
+export const Secondary = Template.bind({  })
+
+export const Fails = Template.bind({});
+Fails.args = {
+    primary: true,
+    action: () => new Promise((res, rej) => {
+        setTimeout(() => { res(false) }, 2000)
+    })
+}
+
+export const Succeeds = Template.bind({});
+Succeeds.args = {
+    primary: true,
+    action: () => new Promise((res, rej) => {
+        setTimeout(() => { res(true) }, 2000)
+    })
+}
+
--- ui/01_atoms/toggle/Toggle.stories.tsx
+++ ui/01_atoms/toggle/Toggle.stories.tsx
@@ -1,5 +1,3 @@
-import React, { useState } from "react";
-
 import { Toggle, ToggleProps } from "./Toggle";
 
 export default {
