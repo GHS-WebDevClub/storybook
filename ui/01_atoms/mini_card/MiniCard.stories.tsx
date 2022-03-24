c36a2c373f4df8b9513e0f80ca65874a52d73ceb
aubincspitzer <59554139+aubincspitzer@users.noreply.github.com>
2022-03-23 21:08:02 -0700

    Added MiniCard component

---
+++ ui/01_atoms/mini_card/MiniCard.stories.tsx
@@ -0,0 +1,34 @@
+import React from "react"
+
+import { MiniCard, MiniCardProps } from "./MiniCard";
+
+export default {
+    title: "Atoms/MiniCard",
+    component: MiniCard,
+};
+
+const Template = (args: MiniCardProps) => <MiniCard {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+    title: "Title",
+    subtitle: "subtitle",
+    secSubtitle: "S.S.",
+    Icon: "verified",
+    iconColor: "red"
+}
+
+export const VerifiedCourse = Template.bind({});
+VerifiedCourse.args = {
+    title: "Jazz Ensemble I",
+    subtitle: "Sessink",
+    Icon: "verified"
+}
+
+export const MultipartAssignment = Template.bind({});
+MultipartAssignment.args = {
+    title: "Great Gatsby Essay",
+    subtitle: "Language Arts",
+    secSubtitle: "3/7",
+    Icon: "warn"
+}
