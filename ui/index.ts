3f4f97aebf5c5e7df95b439f60d0327e3a83c62c
aubincspitzer <59554139+aubincspitzer@users.noreply.github.com>
2022-04-14 13:52:42 -0700

    Added TextInput to be accessible from NPM package

--- ui/index.ts
+++ ui/index.ts
@@ -4,5 +4,6 @@
 export * from "./01_atoms/toggle/Toggle";
 export * from "./01_atoms/icon_button/IconButton";
 export * from "./01_atoms/mini_card/MiniCard";
+export * from "./01_atoms/text_input/TextInput";
 
 /** Add more components here */
